ab948f7908d4cd247ef960df3c5c40a4655b7dd4
Héctor Sab <hector.sab@hotmail.com>
2018-06-06 17:45:28 -0500

    Console log removed

--- js/status.js
+++ js/status.js
@@ -2,16 +2,14 @@ $(document).ready(start);
 
 function start() {
 	var status = ut.getCamStatus();
-	//status = JSON.parse(status);
-	console.log(status['cam1'])
-	
+
 	for (var i=1; i<5; i++) {
 		updateStatus(status['cam'+i.toString()],i);
 	}
 };
 
 function updateStatus(arg,cam) {
-	console.log("hey",arg,cam,'cam-'+cam.toString())
+	// Updates the color of the indicator of the cemera status
 	if(arg==1) {
 		document.getElementById('cam-'+cam.toString()).style.backgroundColor = '#12ba36';
 	} else {
